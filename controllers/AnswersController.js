6e33c168553729bd338142c4146688782b81aef2
Charles Moss & Tim White <student@epicodus.com>
2015-09-24 15:30:25 -0700

    added show/hide comment ui to partial

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -7,11 +7,14 @@ askExpert.controller('AnswersCtrl', function AnswersCtrl($scope, $stateParams, Q
     // };
 
     $scope.addAnswer = function(){
-        $scope.question.answers.push({ aText: $scope.answers.answerText, upvote: 0, comments: []})
+        $scope.question.answers.push({ aText: $scope.answers.answerText, upvote: 0, comments: [], answerId: $scope.answers.length + 1 });
         $scope.answers.answerText = null;
     };
-    // factory.addAnswerComment = function(){
-    //     factory.questions.answers.comments.push({ cText: factory.answers.comments.commentText });
-    //     factory.answers.comments.cText = null;
-    // };
+
+    // $scope.question.answer = UtilitiesFactory.findById(QuestionsFactory.question.answers, $stateParams.answerId);
+
+    $scope.addAnswerComment = function(){
+        $scope.questions.answers.comments.push({ cText: $scope.answers.comments.commentText });
+        $scope.answers.comments.cText = null;
+    };
 });
